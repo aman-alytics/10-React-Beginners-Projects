467a0b80a52ed45d7341392334e6159504cc7bbb
Aman Singh <singhaman131994@gmail.com>
2023-08-04 16:31:16 +0530

    Add files via upload

---
+++ 1 Counter/src/Counter.js
@@ -0,0 +1,22 @@
+import {useState} from "react";
+import "./style.css"
+
+function Counter() {
+    const [count,setCount] = useState(0)
+
+    const increment = () => setCount(count + 1)
+    const decrement = () => setCount(count - 1)
+
+    return <>   
+    <div className="container">
+    <h1 className="number">{count}</h1>
+    </div>
+
+    <section className="btns-container">
+    <button onClick={increment} className="increment">+</button>
+    <button onClick={decrement} className="decrement">-</button>
+    </section>
+    </>
+}
+
+export default Counter;
